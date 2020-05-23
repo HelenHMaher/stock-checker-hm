ab34e7beaa66c8c353a01d9ccd0971c7a62be028
Glitch (stock-checker-hm) <none>
2020-05-23 09:42:47 +0000

    submitted to FCC: styling and security tweeks

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
-const helmet    = require('helmet');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet      = require('helmet');
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
+const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -19,9 +19,10 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
   directives: {
-    defaultSrc: ["'self'"], 
+    defaultSrc: ["'self'"],
     scriptSrc: ["'self'"],
-    styleSrc: ["'self'"]
+    styleSrc: ["'self'"],
+    imgSrc: ["'self'", 'cdn.glitch.com']
   }
 }));
 
